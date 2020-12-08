1e73da5e336e957f58df204746af9edb13083844
rainbowCat <jazziblue@gmail.com>
2020-12-08 15:11:11 -0800

    organize

---
+++ main.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import dataloader
+import pypianoroll
+
+DATA = Path("datasets/lakh-pianoroll")
+IDS_DIR = DATA / "id_lists_lastfm"
+
+DATASET_DIR = DATA / "lpd_5_cleansed"
+
+f = IDS_DIR / "id_list_00s.txt"
+chosen_ids = f.open().readlines()
+print(f"{len(chosen_ids)} are chosen.")
+
+dataset = {}
+for i in chosen_ids:
+    try:
+        npz_dir = dataloader.msd_id_to_dirs(i)
+        npz = dataloader.msd_id_to_pianoroll(npz_dir)
+        dataset[i] = npz
+    except:
+        continue
+
+print(f"{len(dataset)}")
