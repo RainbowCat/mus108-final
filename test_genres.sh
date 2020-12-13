1a55c326311537ccf7d34268f320bbdddd56d088
rainbowCat <jazziblue@gmail.com>
2020-12-13 01:24:39 -0800

    generate genre feature maps

---
+++ test_genres.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+python3 main.py genres/blues/blues.00037.wav genres/blues/blues.00037.wav 1 1 1 1
+python3 main.py genres/classical/classical.00037.wav genres/classical/classical.00037.wav 1 1 1 1
+python3 main.py genres/country/country.00037.wav genres/country/country.00037.wav 1 1 1 1
+python3 main.py genres/disco/disco.00037.wav genres/disco/disco.00037.wav 1 1 1 1
+python3 main.py genres/hiphop/hiphop.00037.wav genres/hiphop/hiphop.00037.wav 1 1 1 1
+python3 main.py genres/jazz/jazz.00037.wav genres/jazz/jazz.00037.wav 1 1 1 1
+python3 main.py genres/metal/metal.00037.wav genres/metal/metal.00037.wav 1 1 1 1
+python3 main.py genres/pop/pop.00037.wav genres/pop/pop.00037.wav 1 1 1 1
+python3 main.py genres/reggae/reggae.00037.wav genres/reggae/reggae.00037.wav 1 1 1 1
+python3 main.py genres/rock/rock.00037.wav genres/rock/rock.00037.wav 1 1 1 1
