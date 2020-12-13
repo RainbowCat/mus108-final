24388ed606cd5fef5b6a22a9da9c75f922e3a80a
rainbowCat <jazziblue@gmail.com>
2020-12-12 20:47:56 -0800

    add more wav files, add hyperparams_sweep

---
+++ hyperparams_sweep.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e2 2000 0.2
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e3 2000 0.2
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e4 2000 0.2
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e5 2000 0.2
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e6 2000 0.2
+
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e2 2000 1
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e3 2000 1
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e4 2000 1
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e5 2000 1
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e6 2000 1
+
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e2 2000 3
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e3 2000 3
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e4 2000 3
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e5 2000 3
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e6 2000 3
+
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e2 2000 7
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e3 2000 7
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e4 2000 7
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e5 2000 7
+python3 main.py data/Debussy_60.wav data/SpringDay_45.wav 1 1e6 2000 7
+
+
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e2 2000 0.2
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e3 2000 0.2
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e4 2000 0.2
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e5 2000 0.2
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e6 2000 0.2
+
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e2 2000 1
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e3 2000 1
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e4 2000 1
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e5 2000 1
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e6 2000 1
+
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e2 2000 3
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e3 2000 3
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e4 2000 3
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e5 2000 3
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e6 2000 3
+
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e2 2000 7
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e3 2000 7
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e4 2000 7
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e5 2000 7
+python3 main.py data/Debussy_60.wav data/HighwayToHell_60.wav 1 1e6 2000 7
+
+# python3 main.py data/Debussy_60.wav data/Rude_30.wav 1 1e4 2000 3
