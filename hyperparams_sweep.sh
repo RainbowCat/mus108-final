979b72857f9b4cdb72394891c0d38f7cfca5b97a
rainbowCat <jazziblue@gmail.com>
2020-12-12 21:27:50 -0800

    add wav

--- hyperparams_sweep.sh
+++ hyperparams_sweep.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python3 main.py data/Debussy_30.wav data/SpringDay_30.wav 1 1e5 2000 0.1
-python3 main.py data/Debussy_30.wav data/SpringDay_30.wav 1e5 1 2000 0.1
+python3 main.py data/Debussy_30.wav data/Rude_30.wav 1 1e5 2000 0.1
+python3 main.py data/Debussy_30.wav data/Rude_30.wav 1e5 1 2000 0.1
 
-python3 main.py data/Debussy_30.wav data/SpringDay_30.wav 1 1e5 2000 1
-python3 main.py data/Debussy_30.wav data/SpringDay_30.wav 1e5 1 2000 1
+python3 main.py data/Debussy_30.wav data/Rude_30.wav 1 1e5 2000 1
+python3 main.py data/Debussy_30.wav data/Rude_30.wav 1e5 1 2000 1
